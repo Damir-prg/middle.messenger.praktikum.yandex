250ba6f0d546cdd72cd36a276b8860abfc4f88d4
Damir Nasirov <krekki01@mail.ru>
2023-12-16 01:06:52 +0300

    refactor: set app starter files

---
+++ src/index.ts
@@ -0,0 +1,3 @@
+import './index.scss';
+
+document.querySelector<HTMLDivElement>('#app')!.innerHTML = `<h1>Спринт 1</h1>`;
--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,11 @@
-import { defineConfig } from "vite";
+import { defineConfig } from 'vite';
 
 export default defineConfig({
   server: {
+    open: '/public/index.html',
     port: 3000,
   },
   build: {
-    outDir: "dist",
+    outDir: 'dist',
   },
 });
