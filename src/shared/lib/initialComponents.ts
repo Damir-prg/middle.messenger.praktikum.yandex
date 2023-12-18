30bf7f1049492821e74713b91ee703b9694c079e
Damir Nasirov <krekki01@mail.ru>
2023-12-18 20:34:43 +0300

    feat: added login form component

--- src/shared/lib/initialComponents.ts
+++ src/shared/lib/initialComponents.ts
@@ -1,6 +1,7 @@
 import Handlebars from 'handlebars';
 import * as Entities from 'entities/index';
 import * as Features from 'features/index';
+import * as Widgets from 'widgets/index';
 
 export const initialComponents = () => {
   Object.entries(Entities).forEach(([name, entity]) => {
@@ -9,5 +10,9 @@ export const initialComponents = () => {
 
   Object.entries(Features).forEach(([name, feature]) => {
     Handlebars.registerPartial(name, feature);
+  });
+
+  Object.entries(Widgets).forEach(([name, widget]) => {
+    Handlebars.registerPartial(name, widget);
   });
 };
