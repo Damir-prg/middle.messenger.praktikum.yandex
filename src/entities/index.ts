84b372a3512c3b5b3e260a961951de9c7e4bf514
Damir Nasirov <krekki01@mail.ru>
2023-12-18 19:56:29 +0300

    refactor: added forgotten import

--- src/entities/index.ts
+++ src/entities/index.ts
@@ -1,4 +1,5 @@
 export { Button } from './button';
 export { Input } from './input';
 export { ErrorPageLayout } from './layots/errorPageLayout';
+export { PageLayout } from './layots/pageLayout';
 export { BlockTitle } from './blockTitle';
