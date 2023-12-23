5f9491c9c8390b710d6802b92a3b5ef34913c890
Damir Nasirov <krekki01@mail.ru>
2023-12-23 20:08:39 +0300

    fix: deleted console.log

--- src/pages/loginPage/index.ts
+++ src/pages/loginPage/index.ts
@@ -2,6 +2,5 @@ import Handlebars from 'handlebars';
 import { default as LoginPageHbs } from './loginPage.hbs?raw';
 
 export const LoginPage = () => {
-  console.log(LoginPageHbs);
   return Handlebars.compile(LoginPageHbs)({});
 };
