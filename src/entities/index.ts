e25c31b43c8594009cf4b4b25a54a2ebb5c65509
Damir Nasirov <krekki01@mail.ru>
2024-01-03 15:31:20 +0300

    refactor

--- src/entities/index.ts
+++ src/entities/index.ts
@@ -1,16 +1,10 @@
 export { Button } from './button';
 export { Input } from './input';
 export { BlockTitle } from './blockTitle';
-export { Form } from './form';
 export { ProfileImage } from './profileImage';
-export { ProfileUl } from './profileUl';
 export { ProfileLi } from './profileLi';
-export { Modal } from './modal';
 export { ChatLi } from './chatLi';
-export { ChatUl } from './chatUl';
-export { ChatDialogHeader } from './chatDialogHeader';
 export { ChatDialogMessagesWrapper } from './chatDialogMessagesWrapper';
-export { ChatDialogFooter } from './chatDialogFooter';
 export { MessageInput } from './MessageInput';
 export { AttachButton } from './attachButton';
 export { UserActionButton } from './userActionButton';
