3a0d4fbb44c0461ec184e0a63525cc7072302055
Damir Nasirov <krekki01@mail.ru>
2024-03-01 02:30:33 +0300

    feat: added hover effect in profile image

--- src/entities/profileImage/profileImage.ts
+++ src/entities/profileImage/profileImage.ts
@@ -3,22 +3,27 @@ import Block from 'shared/core/Block';
 import { CONSTANTS } from 'shared/utilities';
 
 export interface IProfileImageProps {
+  isEdit?: boolean;
   user?: IChatsListItemProps;
 }
 
 export default class ProfileImage extends Block<IProfileImageProps> {
-  constructor() {
+  constructor(props: IProfileImageProps) {
     super({
+      ...props,
       user: CONSTANTS.users[0],
     });
   }
 
   protected render(): string {
-    const { user } = this.props;
+    const { user, isEdit } = this.props;
 
     return `
         <article class="profile-content-image">
-            <img src="${user?.imageUrl}" alt="${user?.name} avatar" class="profile-content-image__image">
+            <div class="profile-content-image__image ${isEdit ? 'profile-content-image__image_pointer' : ''}">
+                <img src="${user?.imageUrl}" alt="${user?.name} avatar" >
+                <div class="profile-content-image__image_pointer__hover">Поменять аватар</div>
+            </div>
             <h1 class="profile-content-image__name">${user?.name}</h1>
         </article>
       `;
