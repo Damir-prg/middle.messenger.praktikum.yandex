d52241ff3edbc7870407c5649a5deaa789f7d85d
Damir Nasirov <krekki01@mail.ru>
2024-03-23 02:51:58 +0300

    feat: added redirect if user is in system

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,22 @@
 import './index.scss';
 import { initialComponents } from 'app/initialComponents';
-import { initialRoutes } from './app/withRoutes';
+import { initialRoutes, routes } from './app/withRoutes';
+import { api } from 'shared/api';
+import Router from 'shared/router/Router';
 
 initialComponents();
 
 initialRoutes();
+
+document.addEventListener('DOMContentLoaded', async () => {
+  try {
+    await api.userInfo();
+  } catch (error) {
+    Router.go(routes.login.route);
+    return;
+  }
+
+  if (window.location.pathname === routes.login.route || window.location.pathname === routes.signUp.route) {
+    Router.go(routes.messenger.route);
+  }
+});
